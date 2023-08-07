51776141308f0829e58cfaf2fc0b231fb6baaf16
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2023-08-07 15:32:17 +0600

    resubmit assignment 2

--- Assignment 2/SymbolTable.py
+++ Assignment 2/SymbolTable.py
@@ -2,10 +2,12 @@ from functools import reduce
 import sys
 
 #Symbol Table
+
 class Node:
-    def __init__(self, name, type, size, dim, loc, addr):
+    #Node class
+    def __init__(self, name, typ, size, dim, loc, addr):
         self.name = name
-        self.type = type
+        self.typ = typ
         self.size = size
         self.dim = dim
         self.loc = loc
@@ -14,10 +16,14 @@ class Node:
 
 class LinkedList:
     def __init__(self) -> None:
+        #An empty Linked List is created with a fake head node.
         self.head = Node("0Head", 0, 0, 0 ,0 ,0)
 
     def add(self, node):
+        #We get the last node in the chain
+        #Then attach the current node to the last node
         t = self.getTail(node.name)
+        #If duplicate nodes with the same name exist, we throw a duplicate error.
         if t:
             t.next = node
             node.next = None
@@ -29,30 +35,35 @@ class LinkedList:
 
         while curr_node.next:
             curr_node = curr_node.next
+            #If duplicate nodes with the same name exist, we return None.
             if curr_node.name == name:
                 return None
             
         return curr_node
     
     def search(self, name):
+        #We iterate through the whole linked list to find a specific node by name.
         curr_node = self.head
-
+        
         while curr_node.next:
             curr_node = curr_node.next
             if curr_node.name == name:
                 return curr_node
         else:
             return None
-        return None
     
     def update(self, name, kwargs):
+        #name -> str
+        #kwargs -> dict
+        #we search and update a node by it's name!
         node = self.search(name)
         if node:
+            #if no nodes exists in of the given name, we do not update!
             for k in kwargs:
                 if k == 'name':
                     node.name = kwargs['name']
-                if k == 'type':
-                    node.type = kwargs['type']
+                if k == 'typ':
+                    node.typ = kwargs['typ']
                 if k == 'size':
                     node.size = kwargs['size']
                 if k == 'loc':
@@ -65,60 +76,79 @@ class LinkedList:
             print('Error NODE not found!')
 
     def delete(self, name):
+        print(f"Deleting: {name}")
         n1 = self.head
         n2 = n1.next
         while n2:
             if n2.name == name:
-                n1.next = n2.next
+                #We cut and stitch the linked list.
+                n1.next = n2.next.next if n2.next else None
+                break
             else:
                 n1 = n2
                 n2 = n2.next
+
     def print(self):
+        #We print all the nodes in this specific linklist chain.
+        #If no node exist in a specific linked link then we print nothing.
         node = self.head.next
         while node:
-            print(node.name, node.type, node.size, node.dim, node.loc, node.addr)
+            print(node.name, node.typ, node.size, node.dim, node.loc, node.addr)
             print("Node Hash: ", Hash_Name(node.name), '\n')
             node = node.next
 
+#A array of Linked List Inititalizing each as empty.
 sym_table = [LinkedList() for i in range(1024)]
 
 
 def Hash_Node(node):
+    #We cumulatively multiply each of the node's name character ascii values 
+    #Then MOD it by 1024 to get the index of the symbol table
     hash_value = reduce(lambda x, y : x*y, [ord(i) for i in node.name])%1024
     return hash_value
 
-def Hash_Node(node):
-    hash_value = reduce(lambda x, y : x*y, [ord(i) for i in node.name])%1024
-    return hash_value
     
 def Hash_Name(name):
+    #same as above but calculates has directly for name
+    #Similar to overloading
     hash_value = reduce(lambda x, y : x*y, [ord(i) for i in name])%1024
     return hash_value
 
-for line in sys.stdin:
-    OP = line.split(', ')[0]
-    #name, type, size, dim, loc, addr
+
+#To input from a file
+# for line in open('input', 'r'):
+#To input from the terminal
+for line in sys.stdin: 
+    line = line.strip()
+    OP = line.split(',')[0]
+    #name, typ, size, dim, loc, addr
     if OP == 'insert':
-        name, type, size, dim, loc, addr = line.split(', ')[1: ]
-        addr = addr.strip()
-        node = Node(name, type, size, dim, loc, addr)
-        hzh = Hash_Node(node)
-        sym_table[hzh].add(node)
+        name, typ, size, dim, loc, addr = line.split(', ')[1: ]
+        node = Node(name, typ, size, dim, loc, addr)
+        print(f"Inserting: {name}")
+        hzh = Hash_Node(node) #Hashed value.
+        sym_table[hzh].add(node) #Inserting the node to the symbol table.
+        
 
     if OP == 'show':
         print("-"*50)
         print("Full Symbol Table")
         print("-"*50)
-        print("name, type, size, dim, loc, addr")
+        print("name, typ, size, dim, loc, addr")
         print("-"*50)
         for i in range(1024):
-            sym_table[i].print()
+            sym_table[i].print() #Printing each chain of the linked lists.
         print("-"*50)
     
     if OP == 'update':
-        name, type, size, dim, loc, addr = line.split(', ')[1: ]
-        addr = addr.strip()
+        name, typ, size, dim, loc, addr = line.split(', ')[1: ]
         hzh = Hash_Name(name)
-        d = {'name':name, 'type':type, 'size':size, 'dim':dim, 'loc':loc, 'addr':addr}
+        d = {'name':name, 'typ':typ, 'size':size, 'dim':dim, 'loc':loc, 'addr':addr}
+        print(f"Updating: {name}")
         sym_table[hzh].update(name, d)
+    
+    if OP == 'delete':
+        name = line.split(', ')[1]
+        hzh = Hash_Name(name)
+        sym_table[hzh].delete(name)
 
